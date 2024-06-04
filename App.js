b0c4b155e66f457fe5d068e639dbc8a92ee79b3e
betsky2112 <roberthussebastians@gmail.com>
2024-06-04 12:46:43 +0700

    Add Weather Search Component

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, StyleSheet} from 'react-native'
+import WeatherSearch from './src/components/weatherSearch'
 
 const App = () => {
 	return (
-		<View>
-			<Text>App</Text>
+		<View style={styles.container}>
+			<WeatherSearch />
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 20,
+	},
+})
 
 export default App
